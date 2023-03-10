e381d14cb5a8881efacbd3902aebc54d175e555a
Michael Brockman <mbrockmandev@gmail.com>
2023-03-10 11:07:18 -0500

    add about, home, notfound pages

---
+++ github-finder/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+function Home() {
+  return (
+    <div>
+      <h1 className='text-6xl'>Welcome</h1>
+    </div>
+  )
+}
+
+// Home.propTypes = {}
+
+export default Home
