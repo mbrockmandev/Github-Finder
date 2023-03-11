958440e04bfd7c99d96daa86ebbfcb91944d20a6
Michael Brockman <mbrockmandev@gmail.com>
2023-03-11 09:26:09 -0500

    update userresults.jsx

--- github-finder/src/pages/Home.jsx
+++ github-finder/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import UserResults from '../components/users/UserResults'
 // import PropTypes from 'prop-types'
 
 function Home() {
   return (
-    <div>
-      <h1 className='text-6xl'>Welcome</h1>
-    </div>
+    <>
+    {/* Search component */}
+      <UserResults />
+    </>
   )
 }
 
